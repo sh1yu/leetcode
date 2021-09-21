ac2af3d1c4e96f3fed32fe6bf8f7f66b864c14a6
psycode <psyucc@163.com>
2021-09-21 23:46:48 +0800

    week7: is valid sudoku.

---
+++ week7/isValidSudoku/isValidSudoku.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/sh1yu/assertion"
+)
+
+func isValidSudoku(board [][]byte) bool {
+	var rows, columns [9][9]int
+	var subboxes [3][3][9]int
+	for i, row := range board {
+		for j, c := range row {
+			if c == '.' {
+				continue
+			}
+			index := c - '1'
+			rows[i][index]++
+			columns[j][index]++
+			subboxes[i/3][j/3][index]++
+			if rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i/3][j/3][index] > 1 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	board := [][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'}}
+	assertion.Equals(true, isValidSudoku(board))
+}
